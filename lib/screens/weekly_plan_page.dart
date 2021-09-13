1df40edb077d69a745459dd32531f68e32679137
JeremyHe <jinyuhe528@gmail.com>
2021-09-13 09:28:37 +0800

    ''

--- lib/screens/weekly_plan_page.dart
+++ lib/screens/weekly_plan_page.dart
@@ -8,32 +8,50 @@ class WeeklyPlanPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: ChangeNotifierProvider<AppState>(
-          create: (_) => AppState(),
-          child: Column(
-            children: [
-              SafeArea(
-                child: Container(
-                  margin: EdgeInsets.only(left: 40, right: 40, top: 40),
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Weekly Diet Plan',
-                            style: TextStyle(
-                                fontSize: 30,
-                                color: Color.fromRGBO(100, 110, 91, 1),
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ],
+        child: Column(
+          children: [
+            SafeArea(
+              child: Container(
+                margin: EdgeInsets.only(left: 40, right: 40, top: 40),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Weekly Diet Plan',
+                          style: TextStyle(
+                              fontSize: 30,
+                              color: Color.fromRGBO(100, 110, 91, 1),
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Icon(
+                          Icons.settings_backup_restore,
+                          size: 50,
+                          color: Color.fromRGBO(100, 110, 91, 1),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        'You have consumed 3500 calories today',
+                        style: TextStyle(
+                          color: Color.fromRGBO(100, 110, 91, 1),
+                          fontSize: 16,
+                        ),
                       ),
-                    ],
-                  ),
+                    )
+                  ],
                 ),
               ),
-              Consumer<AppState>(
+            ),
+            ChangeNotifierProvider<AppState>(
+              create: (_) => AppState(),
+              child: Consumer<AppState>(
                 builder: (context, appState, _) => Container(
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
@@ -77,8 +95,8 @@ class WeeklyPlanPage extends StatelessWidget {
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
